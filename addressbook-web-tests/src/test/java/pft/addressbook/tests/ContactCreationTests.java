a9d7eb8ae998aea2680821ae4667117ae5196619
DaryaManuhina <dmanuhina@gmail.com>
2017-01-22 13:37:37 +0300

    Для тестов создания контакта и группы добавила поиск мах id через лямбда выражение

--- addressbook-web-tests/src/test/java/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/ContactCreationTests.java
@@ -11,34 +11,26 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+  @Test
+  public void testContactCreation() {
 
-        //app.getNavigationHelper().goToHomePage();
-      //  int before = app.getContactHelper().getContactCount();
-        List <ContactData>  before = app.getContactHelper().getContactList();
-        ContactData contact =  new ContactData("Dasha", "Test", "test");
-        app.getContactHelper().createContact(contact);
-        app.getNavigationHelper().goToHomePage();
-        //int after = app.getContactHelper().getContactCount();
-        List <ContactData>  after = app.getContactHelper().getContactList();
-        //Assert.assertEquals(after, before+1);
-        Assert.assertEquals(after.size(), before.size() + 1);
+    //app.getNavigationHelper().goToHomePage();
+    //  int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
+    ContactData contact = new ContactData("Dasha", "Test", "test");
+    app.getContactHelper().createContact(contact);
+    app.getNavigationHelper().goToHomePage();
+    //int after = app.getContactHelper().getContactCount();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    //Assert.assertEquals(after, before+1);
+    Assert.assertEquals(after.size(), before.size() + 1);
 
-        before.add(contact);
-        // нужно узнать макимальный идентификатор maxId, чтобы добавить новую группу  идентификатором maxId+1
-        int max = 0;
-        for (ContactData c : after){
-            if (c.getId() > max) {
-                max =   c.getId();
-            }
-            contact.setId(max);
-        }
-        before.add(contact);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId()); //ищем мах идентификатор
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
 
-    }
+  }
 
 
 }
--- addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -20,16 +21,17 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGropuHelper().getGroupList();
       //  int after = app.getGropuHelper().getGroupCount();
         Assert.assertEquals(after.size(), before.size()+1);
-
-        before.add(group);
-        // нужно узнать макимальный идентификатор maxId, чтобы добавить новую группу  идентификатором maxId+1
-       int max = 0;
+        // Вариант 1. нужно узнать макимальный идентификатор maxId, чтобы добавить новую группу  идентификатором maxId+1
+/*       int max = 0;
        for (GroupData g : after){
            if (g.getId() > max) {
                max =   g.getId();
            }
-           group.setId(max);
+
        }
+        group.setId(max);*/
+       //Вариант 2.  првератим список в поток спавнение с помощью компаратора и ананимные функции
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
